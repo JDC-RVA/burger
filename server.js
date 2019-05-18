ac14a82630d192c7dcec238e9711be65b1469cd9
JDC-RVA <jeremy@firstmotionlc.com>
2019-05-18 01:57:55 -0400

    deloy to heroku

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
+var PORT = process.env.PORT || 8080;
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+// Set Handlebars.
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
