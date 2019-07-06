62f9897f39486a62d1ee4a7ddb1caaf65f5870b9
JDC-RVA <jeremy@firstmotionlc.com>
2019-07-06 00:17:59 -0400

    views added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,17 @@
 var express = require("express");
-var burger = require("../models/burger.js");
 var router = express.Router();
 
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
-    var object = {
+    var hbsObject = {
       burgers: data
     };
-    console.log(object);
-    res.render("index", object);
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
 });
 
